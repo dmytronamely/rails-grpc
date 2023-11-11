05882b19898de570f630fde22513e3f16d0c315f
Dmytro Bielorusov <dmytro.bielorusov@namely.com>
2023-11-11 10:37:04 +0200

    Hf

--- rails-app/config/initializers/grpcw.rb
+++ rails-app/config/initializers/grpcw.rb
@@ -1,5 +1,9 @@
 require_relative '../../lib/grpc/helloworld_services_pb'
 
+GRPC_URL = ENV.fetch('GRPC_URL') do 
+  "#{ENV.fetch('GRPC_SERVER_HOST', 'localhost')}:#{ENV.fetch('GRPC_SERVER_PORT', 50051)}"
+end
+
 class Grpcw
   attr_reader :greeters
 
@@ -8,4 +12,4 @@ class Grpcw
   end
 end
 
-GRPC_STUB = Grpcw.new(stub: Helloworld::Greeter::Stub.new("#{ENV.fetch('GRPC_SERVER_HOST', 'localhost')}:#{ENV.fetch('GRPC_SERVER_PORT', 50051)}", :this_channel_is_insecure)).freeze
+GRPC_STUB = Grpcw.new(stub: Helloworld::Greeter::Stub.new(GRPC_URL, :this_channel_is_insecure)).freeze
