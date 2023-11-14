d0a9560ab541eedc8ff471f98e3fb4f7e81df204
Dmytro Bielorusov <dmytro.bielorusov@namely.com>
2023-11-14 12:13:37 +0200

    HF

--- rails-app/config/puma.rb
+++ rails-app/config/puma.rb
@@ -4,6 +4,35 @@ state_path ENV.fetch('PUMA_STATE_FILE', '/tmp/puma.state')
 workers ENV.fetch('WEB_CONCURRENCY') { 4 }
 port ENV.fetch("PORT") { 3000 }
 
+preload_app!
 # on_worker_boot do
-#   Grpcw.assign_greeters
+#   GRPC.prefork
+# end
+
+# on_booted do
+#   GRPC.prefork
+# end
+
+# on_worker_boot do
+#   GRPC_STUB = Grpcw.new(stub: Helloworld::Greeter::Stub.new(GRPC_URL, :this_channel_is_insecure)).freeze
+# end
+
+# class Client
+#   @@client = Grpcw.new(stub: Helloworld::Greeter::Stub.new(GRPC_URL, :this_channel_is_insecure)).freeze
+
+#   class << self
+#     def stub
+#       @@client
+#     end  
+#   end  
+# end  
+
+# preload_app!
+
+# on_worker_boot do
+# end
+
+# on_booted do
+#   # configuration here
+#   # Client.stub
 # end
